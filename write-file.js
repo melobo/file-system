a4eeaa3624f35d515880ab9a8a43ec3a1f4b4496
melobo <warchocobo@gmail.com>
2020-01-29 18:48:37 -0600

    ADD remove-dir-and-files.js

--- write-file.js
+++ write-file.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-fs.writeFile('test.txt', 'this is a test', err => {
+fs.writeFile('second-dir/content5.txt', 'this is a test', err => {
     if (err) throw err
     console.log('The file has been saved!')
 })
